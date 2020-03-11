955b159b6d61da8da2809f5bc7c66b5d18314414
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2020-03-11 14:58:20 -0700

    bug

--- apps/wall/models.py
+++ apps/wall/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 import re
-from .models import User
+from apps.login_register.models import User
 from django.core.files.storage import FileSystemStorage
 
 # Create your models here
