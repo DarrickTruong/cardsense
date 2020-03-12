6efdf9200f7d77aa35321cc8840b47d328bdfdba
Darrick Truong <truong.darrick@gmail.com>
2020-03-12 14:18:55 -0700

    aws

--- walloffame/settings.py
+++ walloffame/settings.py
@@ -25,9 +25,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 SECRET_KEY = '6rmytx2dc#oz2#bhlo%_-76(ptq2igildrux5wc6f-2ou^-b13'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['52.53.180.33']
 
 
 # Application definition
